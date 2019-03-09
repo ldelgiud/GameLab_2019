96660b1ddec56307df4d349832706b3b939c0056
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2019-03-09 17:29:38 +0100

    Implement Quadtree Constructor

--- Source/Physics/Class1.cs
+++ Source/Physics/Class1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using ECS;
 
 namespace Physics
-{
+{   
     struct Point {
         float x;
         float y;
@@ -23,23 +25,49 @@ namespace Physics
             this.width = width;
             this.height = height;
         }
-        bool containsPoint(Point point) {
+        public bool ContainsPoint(Point point) {
             return false;
         }
-        bool intersectsAABB(AABB other) {
+        public bool ContainsAABB(AABB other)
+        {
+            return false;
+        }
+        public bool IntersectsAABB(AABB other) {
             return false;
         }
     }
     public class Quadtree
     {
+        public const int DefaultMaxDepth = 7;
+        public const int DefaultMaxObjectsPerNode = 25;
         AABB boundary;
         Quadtree NW, NE, SE, SW;
+        //AT the moment all entities store MUST have an AABB (no point insertions)
+        List<Entity> objects = new List<Entity>();
 
         Quadtree(AABB boundary)
         {
             this.boundary = boundary;
         }
 
-        
+        bool Insert(Context ctx, Entity ent) 
+        {
+            //check if AABB is cotained in this quadtree
+            if (!boundary.ContainsAABB(ctx.GetComponent<AABB>(ent))) return false;
+
+            //if this quadtree has children
+            if (NW != null)
+            {
+                //try recursively inserting in children
+                if (NW.Insert(ctx, ent)) return true;
+                if (NE.Insert(ctx, ent)) return true;
+                if (SE.Insert(ctx, ent)) return true;
+                if (SW.Insert(ctx, ent)) return true;
+            }
+
+            //We know the entity is contained, so we can insert it here
+
+            return true;
+        }
     }
 }
