cf9a1a836c482ea48433d0b77da07fe0dcc7d891
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2019-03-09 15:50:37 +0100

    Add Physics library

---
+++ Source/Physics/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Physics
+{
+    struct Point {
+        float x;
+        float y;
+
+        Point(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    //Axis Aligned Bunding Box
+    
+    struct AABB
+    {
+        Point center;
+        float width, height;
+
+        AABB(Point center, float width, float height) {
+            this.center = center;
+            this.width = width;
+            this.height = height;
+        }
+        bool containsPoint(Point point) {
+            return false;
+        }
+        bool intersectsAABB(AABB other) {
+            return false;
+        }
+    }
+    public class Quadtree
+    {
+        AABB boundary;
+        Quadtree NW, NE, SE, SW;
+
+        Quadtree(AABB boundary)
+        {
+            this.boundary = boundary;
+        }
+
+        
+    }
+}
