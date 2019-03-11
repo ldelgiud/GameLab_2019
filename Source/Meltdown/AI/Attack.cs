4a31e8633a9c3491216a9ae1b229decd5556d847
Leonardo Del Giudice <ldelgiud@student.ethz.ch>
2019-03-11 10:45:05 +0100

    Add AI basic Structure

---
+++ Source/Meltdown/AI/Attack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meltdown.AI
+{
+    class Attack : AIState
+    {
+        public override AIState ChangeState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DoAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
