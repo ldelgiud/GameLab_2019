e45d0c311da9b26af954d713d180f7dfba94a5bc
Sven Knobloch <sven98@me.com>
2019-05-06 16:04:26 +0200

    Make interaction distance respect bounding box sizes Reduce interaction distance

--- Source/Hazmat/Utilities/Constants.cs
+++ Source/Hazmat/Utilities/Constants.cs
@@ -56,7 +56,7 @@ namespace Hazmat.Utilities
             public static double MAX_ENERGY = 1000;
         //debug value to stop generating enemies in EnemySpawnSystem
         public static uint MAX_AMOUNT_OF_ENEMIES = 15;
-        public static uint INTERACTION_DISTANCE = 7;
+        public static uint INTERACTION_DISTANCE = 2;
 
         // size of tiles for procedural generation
         public static float TILE_SIZE = 10;
